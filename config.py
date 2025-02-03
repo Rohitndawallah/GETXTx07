9db758d945f983bcd509712f6f943492062dea6e
JEEx07 <141727899+Rohitndawallah@users.noreply.github.com>
2025-02-03 14:13:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "24616136"))
 API_HASH = getenv("API_HASH", "2554bc329f42eb20b0d34bded2847e22")
-BOT_TOKEN = getenv("BOT_TOKEN", "7585239395:AAH_H3bdr7V76AXoO-GjMAlfNPeh5e8uLCQ")
+BOT_TOKEN = getenv("BOT_TOKEN", "7722249850:AAGAQWM2n6FV6mVZb66TzrX_rQChyXjjttk")
 OWNER_ID = int(getenv("OWNER_ID", "7811043854"))
 SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7811043854").split()))
 MONGO_URL = getenv("MONGO_DB", "mongodb+srv://daxxop:daxxop@daxxop.dg3umlc.mongodb.net/?retryWrites=true&w=majority")
